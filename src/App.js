9e962da3b2d26598fcd5c352fe72434ab96d04fd
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-10-08 22:06:12 +0200

    switch to hook-style react code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 import BreadcrumTrail from "./Component/BreadcrumTrail";
 
-class App extends Component{
+function App(props){
+    // it can of course be a state, but here for the test it's just a simple const
+    const content = [
+        (<div>
+            test content 1 <br/>
+            <input type="text" placeholder="dummy input test 1" required/>
+        </div>),
+        "test content 2",
+        (<div>test content 3</div>),
+        (<div>test content 4</div>),
+    ];
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            content:[
-                (<div>
-                    test content 1 <br/>
-                    <input type="text" placeholder="dummy input test 1" required/>
-                </div>),
-                "test content 2",
-                (<div>test content 3</div>),
-                (<div>test content 4</div>),
-            ]
-        };
-    }
-
-    render (){
-        return (
-            <div className="main-container">
-                <BreadcrumTrail
-                    content={this.state.content}
-                    onSubmit={(e)=>{e.preventDefault(); alert('tg');}}
-                />
-            </div>
-        );
-    }
+    return (
+        <div className="main-container">
+            <BreadcrumTrail
+                content={content}
+                onSubmit={(e)=>{e.preventDefault(); alert('tg');}}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/Component/BreadcrumTrail.js
+++ src/Component/BreadcrumTrail.js
@@ -1,29 +1,14 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
+import PropTypes from 'prop-types';
 import './BreadcrumTrail.css';
 
-export default class BreadcrumTrail extends Component{
+export default function BreadcrumTrail(props){
+    const formId = 'form-' + Math.random().toString(36).substr(2, 9);
+    const {onSubmit, content, encType, action, mainColor, mainLightColor, submitting} = props;
+    const [highestTab, setHighestTab] = useState(0);
+    const [currentTab, setCurrentTab] = useState(0);
 
-    formId = 'form-' + Math.random().toString(36).substr(2, 9);
-
-    constructor(props) {
-        super(props);
-        if(!Array.isArray(props.content)){
-            throw new Error('The given content is not valid. Make sure you gave an array.');
-        }
-
-        this.state = {
-            highestTab : 0,
-            currentTab : 0,
-        };
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if(!Array.isArray(this.props.content)){
-            throw new Error('The given content is not valid. Make sure you gave an array.');
-        }
-    }
-
-    handleTabLinkClick = (e,newIndex) => {
+    function handleTabLinkClick(e,newIndex){
         let currentElement = e.currentTarget;
         let animatedElements = document.getElementsByClassName('link-temp-grey-circle');
         for(let i = 0; i < animatedElements.length; i++){
@@ -33,102 +18,106 @@ export default class BreadcrumTrail extends Component{
         newAnimatedElement.className = 'link-temp-grey-circle';
         currentElement.appendChild(newAnimatedElement);
 
-        this.switchTab(newIndex);
+        switchTab(newIndex);
     }
 
-    switchTab = (newIndex) => {
-        let formElement = document.getElementById(this.formId);
+    function switchTab(newIndex){
+        const formElement = document.getElementById(formId);
         if(formElement.reportValidity() !== false){
-            let highestTab = this.state.highestTab;
-            if(newIndex >= highestTab){
-                highestTab = newIndex;
+            let newHighestTab = highestTab;
+            if(newIndex >= newHighestTab){
+                newHighestTab = newIndex;
             }
-
-            this.setState({
-                highestTab : highestTab,
-                currentTab : newIndex
-            });
+            setHighestTab(newHighestTab);
+            setCurrentTab(newIndex);
         }
     }
 
-    updateProgressBarWidth(){
-        let floatWidth = 100 / this.props.content.length;
-        let width = (this.state.currentTab * floatWidth) + (floatWidth / 2);
-        this.setCSSVariableProperty('--progress-bar-width',width + '%');
+    function updateProgressBarWidth(){
+        let floatWidth = 100 / props.content.length;
+        let width = (currentTab * floatWidth) + (floatWidth / 2);
+        setCSSVariableProperty('--progress-bar-width',width + '%');
     }
 
-    setCSSVariableProperty(property,value){
+    function setCSSVariableProperty(property,value){
         document.documentElement.style.setProperty(property,value);
     }
 
-    render(){
-        let {onSubmit,content, encType, action, mainColor, mainLightColor} = this.props;
-        if(onSubmit === undefined || !(onSubmit instanceof Function)){
-            onSubmit = (e)=>{};
-        }
+    let onSubmitFunc = onSubmit;
+    if(onSubmitFunc === undefined || !(onSubmitFunc instanceof Function)){
+        onSubmitFunc = (e)=>{};
+    }
 
-        if(mainColor !== undefined){
-            this.setCSSVariableProperty('--main-color',mainColor);
-        }
-        if(mainLightColor !== undefined){
-            this.setCSSVariableProperty('--main-light-color',mainLightColor);
-        }
+    if(mainColor !== undefined){
+        setCSSVariableProperty('--main-color', mainColor);
+    }
+    if(mainLightColor !== undefined){
+        setCSSVariableProperty('--main-light-color', mainLightColor);
+    }
 
-        if(content.length === 0){
-            return (<div>No data given to form</div>);
-        }
-        this.updateProgressBarWidth();
-        return (
-            <div className='breadcrum-container'>
-                <form id={this.formId} action={action ?? ''} onSubmit={onSubmit} encType={encType ?? ''} >
-                    <div className="breadcrum-header">
-                        <div className="breadcrum-header-progress-bar breadcrum-header-progress-bar-base"/>
-                        <div className="breadcrum-header-progress-bar breadcrum-header-progress-bar-active"/>
-                        {content.map((tab,index) => {
-                            let linkClasses = 'breadcrum-tab-link';
-                            if(index <= this.state.highestTab){
-                                linkClasses += ' breadcrum-link-seen';
-                            }
-                            if(index === parseInt(this.state.currentTab)){
-                                linkClasses += ' breadcrum-link-active';
-                            }
-                            return (
-                                <div className={linkClasses} onClick={(e) => this.handleTabLinkClick(e,index)} key={index}>
-                                    <div className="breadcrum-tab-link-inner-container">
-                                        <span>{index+1}</span>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
+    if(content.length === 0){
+        return (<div>No data given to form</div>);
+    }
+    updateProgressBarWidth();
+    return (
+        <div className='breadcrum-container'>
+            <form id={formId} action={action ?? ''} onSubmit={onSubmitFunc} encType={encType ?? ''} >
+                <div className="breadcrum-header">
+                    <div className="breadcrum-header-progress-bar breadcrum-header-progress-bar-base"/>
+                    <div className="breadcrum-header-progress-bar breadcrum-header-progress-bar-active"/>
                     {content.map((tab,index) => {
-                        let elementClasses = 'breadcrum-tab-content'
-                        if(index !== parseInt(this.state.currentTab)){
-                            elementClasses += ' breadcrum-content-hidden'
+                        let linkClasses = 'breadcrum-tab-link';
+                        if(index <= highestTab){
+                            linkClasses += ' breadcrum-link-seen';
                         }
-
-                        let prevBtn = <div className="dummy-btn">dummy</div>;
-                        let nextBtn = prevBtn;
-                        if(index !== 0){
-                            prevBtn = <div className="breadcrum-button button-previous" onClick={(e) => this.switchTab(index-1)}>Précédent</div>;
-                        }
-                        if(index !== content.length - 1){
-                            nextBtn = <div className="breadcrum-button button-next" onClick={(e) => this.switchTab(index+1)}>Suivant</div>;
-                        }else{
-                            nextBtn = <button className="breadcrum-button breadcrum-submit-button button-next">Valider</button>
+                        if(index === currentTab){
+                            linkClasses += ' breadcrum-link-active';
                         }
                         return (
-                            <div className={elementClasses} key={index}>
-                                <div className="breadcrum-content">{tab}</div>
-                                <div className="breadcrum-content-footer">
-                                    {prevBtn}
-                                    {nextBtn}
+                            <div className={linkClasses} onClick={(e) => handleTabLinkClick(e,index)} key={index}>
+                                <div className="breadcrum-tab-link-inner-container">
+                                    <span>{index+1}</span>
                                 </div>
                             </div>
                         );
                     })}
-                </form>
-            </div>
-        );
-    }
+                </div>
+                {content.map((tab,index) => {
+                    let elementClasses = 'breadcrum-tab-content';
+                    if(index !== currentTab){
+                        elementClasses += ' breadcrum-content-hidden'
+                    }
+
+                    let prevBtn = <div className="dummy-btn">dummy</div>;
+                    let nextBtn = prevBtn;
+                    if(index !== 0){
+                        prevBtn = <div className="breadcrum-button button-previous" onClick={(e) => switchTab(index-1)}>Précédent</div>;
+                    }
+                    if(index !== content.length - 1){
+                        nextBtn = <div className="breadcrum-button button-next" onClick={(e) => switchTab(index+1)}>Suivant</div>;
+                    }else{
+                        nextBtn = <button disabled={submitting ?? false} className="breadcrum-button breadcrum-submit-button button-next">Valider</button>
+                    }
+                    return (
+                        <div className={elementClasses} key={index}>
+                            <div className="breadcrum-content">{tab}</div>
+                            <div className="breadcrum-content-footer">
+                                {prevBtn}
+                                {nextBtn}
+                            </div>
+                        </div>
+                    );
+                })}
+            </form>
+        </div>
+    );
 }
+
+BreadcrumTrail.propTypes = {
+    content: PropTypes.array,
+    onSubmit: PropTypes.func,
+    encType: PropTypes.string,
+    action: PropTypes.string,
+    mainColor: PropTypes.string,
+    mainLightColor: PropTypes.string
+};
